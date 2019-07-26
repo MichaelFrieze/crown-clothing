81cc2e7661bd80a84d5d3c06d6ef323627d8447f
Michael Frieze <michaelfrieze@icloud.com>
2019-07-25 22:31:38 -0400

    wrote some comments explaining withRouter and prop drilling

--- src/components/menu-item/menu-item.js
+++ src/components/menu-item/menu-item.js
@@ -1,6 +1,19 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 
+/* 
+Since only the first (top) component gets access to information like history and match,
+getting this information down to other components can be difficult. 
+You have to keep sending the props down even if the components don't need them. 
+
+But, react-router-dom has a solution to prevent PROP DRILLING
+It's called "withRouter". 
+
+It is a higher order component which is essentially a function
+that takes a component as an argument. 
+Then returns a modified component. 
+*/
+
 import "./menu-item.scss";
 
 const MenuItem = ({ title, imageUrl, size }) => (
