db4ee0c7091c2cadaf69a79c578c2d6ce07719b6
Michael Frieze <michaelfrieze@icloud.com>
2020-03-25 14:02:45 -0400

    removed a random space

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ class App extends React.Component {
 
       this.setState({ currentUser: userAuth });
     });
-  } 
+  }
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
@@ -46,7 +46,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />{" "}
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
