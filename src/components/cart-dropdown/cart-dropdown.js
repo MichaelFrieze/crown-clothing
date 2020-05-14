d9d78959ac2a85f2cd0cf80ebf4559c385e85d2f
Michael Frieze <michaelfrieze@icloud.com>
2020-05-14 15:43:06 -0400

    put a console log in a react component

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -6,12 +6,14 @@ import CartItem from "../cart-item/cart-item";
 
 import "./cart-dropdown.scss";
 
+// how to console log in a react component
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {cartItems.map((cartItem) => (
         <CartItem key={cartItem.id} item={cartItem} />
       ))}
+      {console.log(cartItems)}
     </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
