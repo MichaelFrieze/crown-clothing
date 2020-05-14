ae9880e6cd344a8eaeee6869b8cd61e298763bf4
Michael Frieze <michaelfrieze@icloud.com>
2020-05-14 16:17:44 -0400

    deleted comments and console logs to clean things up

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -17,16 +17,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-// Doing more cool stuff with console log
-const mapStateToProps = (state) =>
-  console.log("The redux state contains:", state) ||
-  console.log("The state is type of:", typeof state) ||
-  console.log("The first cart item name is:", state.cart.cartItems[0].name) ||
-  console.log(
-    "The first cart item is type of:",
-    typeof state.cart.cartItems[0].name
-  ) || {
-    cartItems: state.cart.cartItems,
-  };
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
 
 export default connect(mapStateToProps)(CartDropdown);
