222f9c5c114d31e71d8d7ac4a13838f25654a538
Michael Frieze <michaelfrieze@icloud.com>
2020-05-14 15:50:00 -0400

    removed console log and comments

--- src/components/cart-dropdown/cart-dropdown.js
+++ src/components/cart-dropdown/cart-dropdown.js
@@ -6,14 +6,12 @@ import CartItem from "../cart-item/cart-item";
 
 import "./cart-dropdown.scss";
 
-// how to console log in a react component
 const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
     <div className="cart-items">
       {cartItems.map((cartItem) => (
         <CartItem key={cartItem.id} item={cartItem} />
       ))}
-      {console.log(typeof cartItems, "contains", cartItems)}
     </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
