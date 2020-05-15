02d152189a459f29b40aa70d81278ac4dd17c71a
Michael Frieze <michaelfrieze@icloud.com>
2020-05-14 23:06:22 -0400

    cleaned up comments

--- src/components/cart-icon/cart-icon.js
+++ src/components/cart-icon/cart-icon.js
@@ -18,33 +18,6 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-/* 
-This is called a selector.
-We are writing code that gets a state object
-pulls off a small portion of that state
-Using destructoring:
-{ cart: { cartItems } }
-
-Whenever any reducer updates we are always returning a new object
-whenever we return a new object and redux recomposes and
-rebuilds the entire state object, mapStateToProps is getting
-called every single time. 
-
-Which is always passing in new props into our component. 
-So this is always re rendering our component. 
-
-I am using console log here to show this. 
-
-Even though our cart icon component only cares about cart items
-what ends up happening is whenever we change our state
-like modifying our current user by logging in
-We will see that console log statement here in our cart icon component
-
-This is because our mapStateToProps inside our component 
-is still being called. 
-
-Also, this is another way to console.log but we must use return syntax
-*/
 const mapStateToProps = ({ cart: { cartItems } }) => {
   console.log("I am being called!");
 
