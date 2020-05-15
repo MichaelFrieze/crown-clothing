442f5c11231bcd9f60164104840bfc423c9b179d
Michael Frieze <michaelfrieze@icloud.com>
2020-05-15 01:05:12 -0400

    removed comments

--- src/components/cart-icon/cart-icon.js
+++ src/components/cart-icon/cart-icon.js
@@ -18,37 +18,6 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-/* 
-Now, lets figure out how to prevent this rerender of 
-our component if the state doesn't change. 
-
-This is called memoization
-or the caching of the selectors value
-
-we can achieve this by using a library called 
-RESELECT
-
-caching is just a way to speed up programs
-and hold some piece of data in an easily accessible box
-
-momoization is a specific form of caching. 
-
-This is an example of caching:
-
-let cache = {};
-
-function memoizedAddTo80(n) {
-  if (n in cache) {
-    return cache[n];
-  } else {
-    cache[n] = n + 80;
-    return cache[n];
-  }
-}
-
-console.log("1: ", memoizedAddTo80(5));
-console.log("2: ", memoizedAddTo80(5));
-*/
 const mapStateToProps = ({ cart: { cartItems } }) => ({
   itemCount: cartItems.reduce(
     (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
