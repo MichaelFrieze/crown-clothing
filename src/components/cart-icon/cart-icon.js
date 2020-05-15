b58d86b0ced8807e29bde445398265fab376cfc0
Michael Frieze <michaelfrieze@icloud.com>
2020-05-15 01:04:50 -0400

    added some comments about memoization and chacing in general to prevent rerender of components if state doesn't change | gave code example

--- src/components/cart-icon/cart-icon.js
+++ src/components/cart-icon/cart-icon.js
@@ -18,14 +18,42 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-const mapStateToProps = ({ cart: { cartItems } }) => {
-  return {
-    itemCount: cartItems.reduce(
-      (accumulatedQuantity, cartItem) =>
-        accumulatedQuantity + cartItem.quantity,
-      0
-    ),
-  };
-};
+/* 
+Now, lets figure out how to prevent this rerender of 
+our component if the state doesn't change. 
+
+This is called memoization
+or the caching of the selectors value
+
+we can achieve this by using a library called 
+RESELECT
+
+caching is just a way to speed up programs
+and hold some piece of data in an easily accessible box
+
+momoization is a specific form of caching. 
+
+This is an example of caching:
+
+let cache = {};
+
+function memoizedAddTo80(n) {
+  if (n in cache) {
+    return cache[n];
+  } else {
+    cache[n] = n + 80;
+    return cache[n];
+  }
+}
+
+console.log("1: ", memoizedAddTo80(5));
+console.log("2: ", memoizedAddTo80(5));
+*/
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
+    0
+  ),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
