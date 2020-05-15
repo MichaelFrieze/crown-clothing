0ebc55d91324e94c92a7458c536fa5c946b4ee96
Michael Frieze <michaelfrieze@icloud.com>
2020-05-14 20:33:22 -0400

    selectors in redux | updating cart icon to show quantity | detailed comments explaining mapStateToProps rerendering the component

--- src/components/cart-icon/cart-icon.js
+++ src/components/cart-icon/cart-icon.js
@@ -7,10 +7,10 @@ import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import "./cart-icon.scss";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
@@ -18,4 +18,43 @@ const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+/* 
+This is called a selector.
+We are writing code that gets a state object
+pulls off a small portion of that state
+Using destructoring:
+{ cart: { cartItems } }
+
+Whenever any reducer updates we are always returning a new object
+whenever we return a new object and redux recomposes and
+rebuilds the entire state object, mapStateToProps is getting
+called every single time. 
+
+Which is always passing in new props into our component. 
+So this is always re rendering our component. 
+
+I am using console log here to show this. 
+
+Even though our cart icon component only cares about cart items
+what ends up happening is whenever we change our state
+like modifying our current user by logging in
+We will see that console log statement here in our cart icon component
+
+This is because our mapStateToProps inside our component 
+is still being called. 
+
+Also, this is another way to console.log but we must use return syntax
+*/
+const mapStateToProps = ({ cart: { cartItems } }) => {
+  console.log("I am being called!");
+
+  return {
+    itemCount: cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    ),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
