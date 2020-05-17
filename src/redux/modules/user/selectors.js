cfd2fb005397430bdf6d4a96a430c6b82adcff6a
Michael Frieze <michaelfrieze@icloud.com>
2020-05-17 19:48:53 -0400

    added the directory data to redux instead of keeping it in our directory component | changed directory component from class to functional | created a directory selector with reselect

--- src/redux/modules/user/selectors.js
+++ src/redux/modules/user/selectors.js
@@ -6,16 +6,3 @@ export const selectCurrentUser = createSelector(
   [selectUser],
   (user) => user.currentUser
 );
-
-/* 
-We can do it this way too (instead of using brackets)
-
-const selectUser = (state) => state.user;
-const selectCart = (state) => state.cart;
-
-export const selectCurrentUser = createSelector(
-  selectUser,
-  selectCart,
-  (user) => user.currentUser
-);
- */
