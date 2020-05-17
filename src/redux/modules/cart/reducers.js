e6cb5013446376fd2960a4001ee0747658d3bf98
Michael Frieze <michaelfrieze@icloud.com>
2020-05-16 22:08:35 -0400

    finished cart functionality | remove items from cart and remove items from checkout

--- src/redux/modules/cart/reducers.js
+++ src/redux/modules/cart/reducers.js
@@ -1,5 +1,5 @@
-import types from "./types";
-import * as utils from "./utils";
+import CartActionTypes from "./types";
+import { addItemToCart, removeItemFromCart } from "./utils";
 
 const INITIAL_STATE = {
   hidden: true,
@@ -8,15 +8,27 @@ const INITIAL_STATE = {
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case types.TOGGLE_CART_HIDDEN:
+    case CartActionTypes.TOGGLE_CART_HIDDEN:
       return {
         ...state,
         hidden: !state.hidden,
       };
-    case types.ADD_ITEM:
+    case CartActionTypes.ADD_ITEM:
       return {
         ...state,
-        cartItems: utils.addItemToCart(state.cartItems, action.payload),
+        cartItems: addItemToCart(state.cartItems, action.payload),
+      };
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload),
+      };
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          (cartItem) => cartItem.id !== action.payload.id
+        ),
       };
     default:
       return state;
