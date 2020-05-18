f25e95cb73c0a907ddede3e59bc6e102acd04af5
Michael Frieze <michaelfrieze@icloud.com>
2020-05-17 20:23:29 -0400

    added shop data to redux instead of keeping it with our shop page component | changed shop page from class to functional | created a collections selector with reselect

--- src/pages/shop/index.js
+++ src/pages/shop/index.js
@@ -1,28 +1,21 @@
 import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import SHOP_DATA from "./data";
+import { selectCollections } from "../../redux/modules/shop/selectors";
 
 import CollectionPreview from "../../components/collection-preview/collection-preview";
 
-class Shop extends React.Component {
-  constructor(props) {
-    super(props);
+const Shop = ({ collections }) => (
+  <div className="shop-page">
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
+  </div>
+);
 
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
-
-export default Shop;
+export default connect(mapStateToProps)(Shop);
