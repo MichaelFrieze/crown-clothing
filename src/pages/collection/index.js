9351011b7893d0874fe337eebacb69a53b606148
Michael Frieze <michaelfrieze@icloud.com>
2020-05-18 12:18:43 -0400

    removed comments and console log

--- src/pages/collection/index.js
+++ src/pages/collection/index.js
@@ -1,23 +1,20 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import CollectionItem from "../../components/collection-item/collection-item";
+// import CollectionItem from "../../components/collection-item/collection-item";
 
 import { selectCollection } from "../../redux/modules/shop/selectors";
 
 import "./index.scss";
 
-const CollectionPage = ({ collection }) =>
-  console.log(collection) || (
-    <div className="collection-page">
-      <h2>CollectionPage</h2>
-    </div>
-  );
+const CollectionPage = ({ collection }) => (
+  <div className="collection-page">
+    <h2>CollectionPage</h2>
+  </div>
+);
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state),
 });
-
-// This is an example of currying so is the selector
 
 export default connect(mapStateToProps)(CollectionPage);
