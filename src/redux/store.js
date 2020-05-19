7d662058044fed99d9822c68f9c887266de6fab7
Michael Frieze <michaelfrieze@icloud.com>
2020-05-19 03:14:39 -0400

    removed logger from production

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,11 @@ import logger from "redux-logger";
 
 import * as reducers from "./modules";
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 const persistConfig = {
   key: "root",
