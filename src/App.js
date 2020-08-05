777f79edcc1cd92c5319a7510b36e6a4e49c9f82
Michael Frieze <michaelfrieze@icloud.com>
2020-08-05 03:15:02 -0400

    removed console log

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ class App extends React.Component {
 
   componentDidMount() {
     const { setCurrentUser } = this.props;
-    console.log("testing");
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
