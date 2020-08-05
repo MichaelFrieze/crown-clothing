e5cdf4c04ec4727dffcc1882779630d719d25717
Michael Frieze <michaelfrieze@icloud.com>
2020-08-05 03:14:29 -0400

    just testing my ssh key for git

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ class App extends React.Component {
 
   componentDidMount() {
     const { setCurrentUser } = this.props;
+    console.log("testing");
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
