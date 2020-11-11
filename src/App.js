fdbde0bde12d856812c46746f21382a5c82097e0
Michael Frieze <michaelfrieze@icloud.com>
2020-11-11 13:55:28 -0500

    manually added shop data into firebase

--- src/App.js
+++ src/App.js
@@ -11,15 +11,22 @@ import Login from "./pages/login";
 import Checkout from "./pages/checkout";
 
 import Header from "./components/header/header";
-import { auth, createUserProfileDocument } from "./firebase/firebase";
+
+import {
+  auth,
+  createUserProfileDocument,
+  addCollectionAndDocuments,
+} from "./firebase/firebase";
+
 import { setCurrentUser } from "./redux/modules/user/actions";
 import { selectCurrentUser } from "./redux/modules/user/selectors";
+import { selectCollectionsForPreview } from "./redux/modules/shop/selectors";
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
+    const { setCurrentUser, collectionsArray } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
@@ -34,6 +41,10 @@ class App extends React.Component {
       }
 
       setCurrentUser(userAuth);
+      addCollectionAndDocuments(
+        "collections",
+        collectionsArray.map(({ title, items }) => ({ title, items }))
+      );
     });
   }
 
@@ -64,6 +75,7 @@ class App extends React.Component {
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
+  collectionsArray: selectCollectionsForPreview,
 });
 
 const mapDispatchToProps = (dispatch) => ({
